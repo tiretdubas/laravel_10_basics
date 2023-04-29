8bed759d4ee4ee316fb1dadb58fbe6e8dcd95f57
Steven Sil <infoprog.tuto@gmail.com>
2023-04-29 09:37:53 +0200

    épisode 16

--- resources/views/components/post.blade.php
+++ resources/views/components/post.blade.php
@@ -27,7 +27,7 @@
             Lire l'article
         </a>
         @else
-        <time class="text-xs text-slate-400" datetime="{{ $post->created_at }}">{{ $post->created_at }}</time>
+        <time class="text-xs text-slate-400" datetime="{{ $post->created_at }}">@datetime($post->created_at)</time>
         @endif
     </div>
 </article>
